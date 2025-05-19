3d6eefbd650d44110fdfea384dede8fa09bf4bab
Beletatti <kaiquebeletatti@gmail.com>
2025-05-19 15:30:28 -0300

    persisting products in the database and get,put, post, delete methods

--- src/main/java/github/beletatti/productapi/controller/ProductController.java
+++ src/main/java/github/beletatti/productapi/controller/ProductController.java
@@ -1,18 +1,52 @@
 package github.beletatti.productapi.controller;
 
 import github.beletatti.productapi.model.Product;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import github.beletatti.productapi.repository.ProductRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("products")
 public class ProductController {
 
+    private ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @PostMapping
     public Product save(@RequestBody Product product) {
         System.out.println("Produto recebido: " + product);
+
+        var id = UUID.randomUUID().toString();
+        product.setId(id);
+
+        productRepository.save(product);
         return product;
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable("id") String id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        productRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void update(@PathVariable("id") String id, @RequestBody Product product) {
+        product.setId(id);
+        productRepository.save(product);
+    }
+
+    @GetMapping
+    public List<Product> search(@RequestParam("name") String name) {
+        return productRepository.findByName(name);
     }
 }
